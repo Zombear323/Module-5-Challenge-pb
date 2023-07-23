590895bc2fcf7e0c1f79b34ba53ebcfb43371950
Zombear323 <129801201+Zombear323@users.noreply.github.com>
2023-07-23 03:57:08 -0400

    for loop created to update with change in hour

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,32 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
-  
-});
+var timeSlot1 = $('#hour-9')
+var timeSlot2 = $('#hour-10')
+var timeSlot3 = $('#hour-11')
+var timeSlot4 = $('#hour-12')
+var timeSlot5 = $('#hour-1')
+var timeSlot6 = $('#hour-2')
+var timeSlot7 = $('#hour-3')
+var timeSlot8 = $('#hour-4')
+var timeSlot9 = $('#hour-5')
+
+var timeLine = [timeSlot1, timeSlot2, timeSlot3, timeSlot4, timeSlot5, timeSlot6, timeSlot7, timeSlot8, timeSlot9]
+
+
+var time = dayjs().format('H');
+var date = dayjs().format('MMMM D, YYYY');
+console.log(time);
+console.log(date);
+
+for (let index = 9; index < time; index++) {
+  var done = index - 9
+  console.log(done)
+  // timeLine[done].addClass('past');  
+};
+
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
